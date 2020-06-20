256f0d61010545eac1c0f4b6f5c29e3031216965
Rohan Tamhankar <curiousrohan1@gmail.com>
2020-06-20 16:34:08 -0700

    Initial commit of create-recipe api

---
+++ src/main/java/org/dishorg/dishorg/DishorgApplication.java
@@ -0,0 +1,13 @@
+package org.dishorg.dishorg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DishorgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DishorgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/dishorg/dishorg/DishorgApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dishorg.dishorg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DishorgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
