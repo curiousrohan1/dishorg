35b9dff80333bdcc6ab289d95ad54208cd76cecc
Rohan Tamhankar <curiousrohan1@gmail.com>
2020-07-09 15:35:39 -0700

    Added delete feature for recipes.

--- src/main/resources/public/js/recipes.js
+++ src/main/resources/public/js/recipes.js
@@ -1,11 +1,11 @@
 var currentRec = null;
-var currentIdx=null;
+var currentIdx = null;
 function createIngDiv(idx) {
     return `
        <div id="ingDiv${idx}">
-            <input id="quantity${idx}" placeholder="Quantity" type="text">
-            <input id="unit${idx}" placeholder="Unit" type="text">
-            <input id="name${idx}" placeholder="Name" type="text">
+            <input id="quantity${idx}" placeholder="Quantity" type="text"class="inputIngInfo"data-idx="${idx}">
+            <input id="unit${idx}" placeholder="Unit" type="text"class="inputIngInfo"data-idx="${idx}">
+            <input id="name${idx}" placeholder="Name" type="text" data-idx="${idx}" class="inputIngInfo">
             <button class="btn btn-dark apply" id="addIng${idx}" data-idx="${idx}" type="button"><img src="images/apply.png"style="width:30px;height:30px;"></button>
             <button class="btn btn-light cancel" id="cancelIng${idx}" data-idx="${idx}" type="button"><img src="images/cancel.jpg"style="width:30px;height:30px;"></button>
         </div>`
@@ -15,8 +15,8 @@ function ingCancel(idx) {
     if (idx === "") {
         $("#ingDiv").hide();
     } else {
-        $("#ingEdit"+idx).hide();
-        $("#ingLineContainer"+idx).show();
+        $("#ingEdit" + idx).hide();
+        $("#ingLineContainer" + idx).show();
     }
 }
 
@@ -25,7 +25,7 @@ function ingApply(idx) {
     var name = $("#name" + idx).val();
     var unit = $("#unit" + idx).val();
     var line = quantity + " " + unit + " of " + name;
-    if(idx === ""){
+    if (idx === "") {
         currentRec.ingredients.push({ quantity: quantity, name: name, unit: unit });
         $("#unit").val("");
         $("#name").val("");
@@ -34,7 +34,7 @@ function ingApply(idx) {
         currentRec.ingredients[idx] = { quantity: quantity, name: name, unit: unit };
     }
     $.ajax({
-        type:"PUT", url: "/recipes/"+currentRec.id, data: JSON.stringify(currentRec), contentType: "application/json", dataType: "json", success: successOnAjaxOfRecipe
+        type: "PUT", url: "/recipes/" + currentRec.id, data: JSON.stringify(currentRec), contentType: "application/json", dataType: "json", success: successOnAjaxOfRecipe
     });
 }
 
@@ -62,47 +62,47 @@ function successOnAjaxOfRecipe(recipe, status) {
                 ${line}
             </li>`)
         $("#recipe-details").append(`<li id="ingEdit${idx}">` + createIngDiv(idx) + '</li>');
-        $("#ingEdit"+idx).hide();
-        $("#quantity"+idx).val(quantity);
-        $("#unit"+idx).val(unit);
-        $("#name"+idx).val(name);
-        $("button.edit-recipes").prop("disabled", false );
+        $("#ingEdit" + idx).hide();
+        $("#quantity" + idx).val(quantity);
+        $("#unit" + idx).val(unit);
+        $("#name" + idx).val(name);
+        $("button.edit-recipes").prop("disabled", false);
     });
-    currentRec=recipe;
+    currentRec = recipe;
     $("#recTitle").text(currentRec.name);
-    $("button.edit-recipes").click(function(){
-        var idx=$(this).data("idx");
-        var ing=currentRec.ingredients[idx];
-        $("#quantity"+idx).val(ing.quantity);
-        $("#unit"+idx).val(ing.unit);
-        $("#name"+idx).val(ing.name);
-        $("button.edit-recipes").prop("disabled", true );
-        $("#ingEdit"+idx).show();
-        $("#ingLineContainer"+idx).hide();
+    $("button.edit-recipes").click(function () {
+        var idx = $(this).data("idx");
+        var ing = currentRec.ingredients[idx];
+        $("#quantity" + idx).val(ing.quantity);
+        $("#unit" + idx).val(ing.unit);
+        $("#name" + idx).val(ing.name);
+        $("button.edit-recipes").prop("disabled", true);
+        $("#ingEdit" + idx).show();
+        $("#ingLineContainer" + idx).hide();
     });
-    $("li div button.cancel").click(function(){
-        $("button.edit-recipes").prop("disabled", false );
-        var idx=$(this).data("idx");
+    $("li div button.cancel").click(function () {
+        $("button.edit-recipes").prop("disabled", false);
+        var idx = $(this).data("idx");
         ingCancel(idx);
     });
-    $("li div button.apply").click(function(){
-        var idx=$(this).data("idx");
+    $("li div button.apply").click(function () {
+        var idx = $(this).data("idx");
         ingApply(idx);
     });
-    $("button.openModal").click(function(){
-        currentIdx=$(this).data("idx");
+    $("button.openModal").click(function () {
+        currentIdx = $(this).data("idx");
     })
-    $("#continueDel").click(function(){
+    $("#continueDel").click(function () {
         var ing = currentRec.ingredients[currentIdx]
         var newIngArr = [];
-        currentRec.ingredients.forEach(function(ingredient, idx, arr){
-            if(ing!==ingredient){
-                newIngArr.push({quantity:ingredient.quantity,unit:ingredient.unit,name:ingredient.name})
+        currentRec.ingredients.forEach(function (ingredient, idx, arr) {
+            if (ing !== ingredient) {
+                newIngArr.push({ quantity: ingredient.quantity, unit: ingredient.unit, name: ingredient.name })
             }
         });
-        currentRec.ingredients=newIngArr;
+        currentRec.ingredients = newIngArr;
         $.ajax({
-            type:"PUT", url: "/recipes/"+currentRec.id, data: JSON.stringify(currentRec), contentType: "application/json", dataType: "json", success: successOnAjaxOfRecipe
+            type: "PUT", url: "/recipes/" + currentRec.id, data: JSON.stringify(currentRec), contentType: "application/json", dataType: "json", success: successOnAjaxOfRecipe
         });
     });
 }
@@ -115,28 +115,17 @@ function addRecHandler(matchingButtons) {
         $.get("/recipes/" + $(this).data("id"), successOnAjaxOfRecipe);
     });
 }
-$(document).ready(function() {
+function virtualRestart(data,status) {
     $("#renamed-recipe-name").hide();
-    $("#recipe-details-container").append(createIngDiv(""));
-    $("#addIng").click(function() {
-        ingApply("");
-    });
-    $("#cancelIng").click(function() {
-        ingCancel("");
-    });
     $("#unit").val("");
     $("#name").val("");
     $("#quantity").val("");
     $("#recDiv").hide();
     $("#ingDiv").hide();
     $("#recipe-details-container").hide();
-    $("#new-recipe-name").keypress(function(event) {
-        if (event.keyCode === 13) {
-            $("#addRec").click();
-        }
-    });
     // On page load, gets the recipe list, and appends as buttons to a ul in left pane.
     $.get("/recipes", function (recipeList, status) {
+        $("#recipe-list").empty();
         recipeList.forEach(function (recipe, idx, arr) {
             $("#recipe-list").append(
                 '<button class="recipe-list list-group-item list-group-item-action " type="button" data-id="'
@@ -144,6 +133,43 @@ $(document).ready(function() {
         });
         addRecHandler($("button.recipe-list"));
     }, "json");
+    currentRec = null;
+    currentIdx = null;
+}
+
+$(document).ready(function () {
+    virtualRestart(null,null);
+    $("#recipe-details-container").append(createIngDiv(""));
+    $("#addIng").click(function () {
+        ingApply("");
+    });
+    $("#cancelIng").click(function () {
+        ingCancel("");
+    });
+    $("#new-recipe-name").keypress(function (event) {
+        if (event.keyCode === 13) {
+            $("#addRec").click();
+        }
+    });
+    $("#renameRecInput").keypress(function (event) {
+        if (event.keyCode === 13) {
+            $("#applyRecRename").click();
+        }
+    });
+    $("input.inputIngInfo").keypress(function (event) {
+        var idx = $(this).data("idx");
+        if ($("#unit" + idx).val() !== "") {
+            if ($("#quantity" + idx).val() !== "") {
+                if ($("#name" + idx).val() !== "") {
+                    if (event.keyCode === 13) {
+                        $("#addIng" + idx).click();
+                    }
+                }
+            }
+        }
+    });
+
+
     $("#plusRec").click(function () {
         $("#new-recipe-name").val("");
         $("#recDiv").show();
@@ -157,7 +183,7 @@ $(document).ready(function() {
                 $("#recipe-list").append(
                     '<button class="recipe-list list-group-item list-group-item-action " type="button" data-id="'
                     + data.id + '">' + data.name + '</button>');
-                var lastButton=$("button.recipe-list.list-group-item-action:last-child");
+                var lastButton = $("button.recipe-list.list-group-item-action:last-child");
                 addRecHandler(lastButton);
                 lastButton.trigger("click");
             }
@@ -169,25 +195,33 @@ $(document).ready(function() {
         $("#quantity").val("");
         $("#ingDiv").show();
     });
-    $("#editRecName").click(function(){
+    $("#delRec").click(function () {
+        $.ajax({
+            type: "DELETE", url: "/recipes/" + currentRec.id, dataType: "json",   statusCode: {
+                200: virtualRestart
+            }
+        });
+    })
+    $("#editRecName").click(function () {
         $("#recTitle").hide();
         $("#editRecName").hide();
         $("#renamed-recipe-name").show();
         $("#renameRecInput").val($("#recTitle").text());
     })
-    $("#applyRecRename").click(function(){
-        currentRec.name= $("#renameRecInput").val();
+    $("#applyRecRename").click(function () {
+        currentRec.name = $("#renameRecInput").val();
         $.post({
             url: "recipes", data: JSON.stringify(currentRec), contentType: "application/json", dataType: "json", success: function (data, status) {
                 $("button.recipe-list.active").text(data.name);
             }
-         });
+        });
         $("#recTitle").show();
         $("#editRecName").show();
         $("#renamed-recipe-name").hide();
         $("#renameRecInput").val($("#recTitle").text());
+        $("#recTitle").text(currentRec.name);
     })
-    $("#cancelRecRename").click(function(){
+    $("#cancelRecRename").click(function () {
         $("#recTitle").show();
         $("#editRecName").show();
         $("#renamed-recipe-name").hide();
