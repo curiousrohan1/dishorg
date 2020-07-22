7b5dd677acb4382deb9b973ea0659e7c2d45583d
Rohan Tamhankar <curiousrohan1@gmail.com>
2020-07-22 09:25:55 -0700

    * Added an enum for the list of units. * Linted again.

--- src/main/resources/public/js/recipes.js
+++ src/main/resources/public/js/recipes.js
@@ -1,74 +1,11 @@
 let currentRec = null;
 let currentIdx = null;
+let unitList = null
 
 /*
-  successOnAjaxOfRecipe
-
-  Clear recipe details and then for each ingredient, append it to recipe details, along with an
-  edit and delete button. Then, it appends an ingDiv for editing purposes in the future.
-
-  Parameters:
-  recipe - The recipe object obtained in an ajax (PUT, GET, POST) response
-  status - string status; should be "success"
 */
-function successOnAjaxOfRecipe(recipe) {
-  $('#recipe-details').empty();
-  currentRec = recipe;
-  $('#rec-title').text(currentRec.name);
-  currentRec.ingredients.forEach((ingredient, idx) => {
-    const { quantity } = ingredient;
-    const { name } = ingredient;
-    const { unit } = ingredient;
-    let line = '';
-    if (unit === '') {
-      line = `${quantity} ${name}`;
-    } else {
-      line = `${quantity} ${unit} of ${name}`;
-    }
-    $('#recipe-details').append(`
-        <li id="ing-line-container${idx}">
-            <button class="edit-recipes btn" id="edit-ing${idx}" data-idx="${idx}">
-                <img src="images/edit.jpg"style="width:30px;height:30px;">
-            </button>
-            <button type="button" class="btn openModal" data-toggle="modal" data-idx="${idx}"
-                    id="open-modal${idx}" data-target="#cont-del-modal">
-                <img src="images/del.png"style="width:30px;height:30px;">
-            </button>
-            ${line}
-        </li>`);
-    $('#recipe-details').append(`<li id="ing-edit${idx}">${createIngDiv(idx)}</li>`);
-    $(`#ing-edit${idx}`).hide();
-    $('button.edit-recipes').prop('disabled', false);
-    $(`#quantity${idx}, #unit${idx}, #name${idx}`).keypress((event) => {
-      if ($(`#quantity${idx}`).val() !== '' && $(`#name${idx}`).val() !== ''
-          && event.keyCode === 13) {
-        $(`#add-ing${idx}`).click();
-      }
-    });
-  });
-  $('button.edit-recipes').click(function () {
-    const idx = $(this).data('idx');
-    const ing = currentRec.ingredients[idx];
-    // Populate the ingredient's input fields with the current values from currentRec and then show
-    // the input fields; also hide the "line".
-    $(`#quantity${idx}`).val(ing.quantity);
-    $(`#unit${idx}`).val(ing.unit);
-    $(`#name${idx}`).val(ing.name);
-    $('button.edit-recipes').prop('disabled', true);
-    $(`#ing-edit${idx}`).show();
-    $(`#ing-line-container${idx}`).hide();
-    $(`#quantity${idx}`).focus();
-  });
-  $('li > div > button.cancel').click(function () {
-    $('button.edit-recipes').prop('disabled', false);
-    ingCancel($(this).data('idx'));
-  });
-  $('li > div > button.apply').click(function () {
-    ingApply($(this).data('idx'));
-  });
-  $('button.openModal').click(function () {
-    currentIdx = $(this).data('idx');
-  });
+function populateUnitDropdown(idx){
+  $(`#dropdown${idx}`).text($(`#link${idx}`).text())
 }
 
 /*
@@ -81,19 +18,27 @@ function successOnAjaxOfRecipe(recipe) {
   idx - The index of the ingDiv created
 */
 function createIngDiv(idx) {
-  return `
-   <div id="ing-div${idx}">
-      <input id="quantity${idx}" placeholder="Quantity" type="text" class="inputIngInfo"
-             data-idx="${idx}">
-      <input id="unit${idx}" placeholder="Unit" type="text" class="inputIngInfo" data-idx="${idx}">
-      <input id="name${idx}" placeholder="Name" type="text" data-idx="${idx}" class="inputIngInfo">
-      <button class="btn btn-dark apply" id="add-ing${idx}" data-idx="${idx}" type="button">
-        <img src="images/apply.png"style="width:30px;height:30px;">
-      </button>
-      <button class="btn btn-light cancel" id="cancel-ing${idx}" data-idx="${idx}" type="button">
-        <img src="images/cancel.jpg"style="width:30px;height:30px;">
-      </button>
-    </div>`;
+  var ingDiv=`
+    <div id="ing-div${idx}">
+       <input id="quantity${idx}" placeholder="Quantity" type="text" class="inputIngInfo" data-idx="${idx}">
+       <div class="btn-group"id="unit${idx}">
+           <button type="button" class="btn"id="dropdown${idx}"></button>
+           <button type="button" class="btn dropdown-toggle dropdown-toggle-split" data-toggle="dropdown">
+           </button>
+           <div class="dropdown-menu" id="unit-dropdown${idx}">
+              <a class="dropdown-item" id="link${idx}" href="populate(idx)">Link 1</a>
+           </div>
+         </div>
+       <input id="name${idx}" placeholder="Name" type="text" data-idx="${idx}" class="inputIngInfo">
+       <button class="btn btn-dark apply" id="add-ing${idx}" data-idx="${idx}" type="button">
+         <img src="images/apply.png"style="width:30px;height:30px;">
+       </button>
+       <button class="btn btn-light cancel" id="cancel-ing${idx}" data-idx="${idx}" type="button">
+         <img src="images/cancel.jpg"style="width:30px;height:30px;">
+       </button>
+     </div>`;
+//  unitList.forEach()
+  return ingDiv;
 }
 
 /*
@@ -127,12 +72,7 @@ function ingApply(idx) {
   const quantity = $(`#quantity${idx}`).val();
   const name = $(`#name${idx}`).val();
   const unit = $(`#unit${idx}`).val();
-  let line = '';
-  if (unit === '') {
-    line = `${quantity} ${name}`;
-  } else {
-    line = `${quantity} ${unit} of ${name}`;
-  }
+  const line = `${quantity} ${unit} of ${name}`;
   if (idx === '') {
     // Idx is empty, so we are adding, not editing.
     currentRec.ingredients.push({ quantity, name, unit });
@@ -149,24 +89,86 @@ function ingApply(idx) {
     data: JSON.stringify(currentRec),
     contentType: 'application/json',
     dataType: 'json',
-    success: successOnAjaxOfRecipe,
-  });
-  $('#quantity').focus();
+  });// .done(function (data) {
+//    successOnAjaxOfRecipe(data);
+//  }).fail(function (jqXHR, textStatus) {
+//    console.log("ERROR");
+//  });
 }
 
 /*
-  recipeButtonCallback
+  successOnAjaxOfRecipe
 
-  The callback of the recipe buttons.
+  Clear recipe details and then for each ingredient, append it to recipe details, along with an
+  edit and delete button. Then, it appends an ingDiv for editing purposes in the future.
 
-  Parameters: none.
+  Parameters:
+  recipe - The recipe object obtained in an ajax (PUT, GET, POST) response
+  status - string status; should be "success"
 */
-function recipeButtonCallback() {
-  $('#recipe-details-container').show();
-  $('button.active.recipe-list').removeClass('active');
-  $(this).addClass('active');
-  $.get(`/recipes/${$(this).data('id')}`, successOnAjaxOfRecipe);
-  $('#ing-div').hide();
+function successOnAjaxOfRecipe(recipe, status) {
+  $('#recipe-details').empty();
+  currentRec = recipe;
+  $('#rec-title').text(currentRec.name);
+  currentRec.ingredients.forEach((ingredient, idx, arr) => {
+    const { quantity } = ingredient;
+    const { name } = ingredient;
+    const { unit } = ingredient;
+    const line = `${quantity} ${unit} of ${name}`;
+    $('#recipe-details').append(`
+        <li id="ing-line-container${idx}">
+            <button class="edit-recipes btn" id="edit-ing${idx}" data-idx="${idx}">
+                <img src="images/edit.jpg"style="width:30px;height:30px;">
+            </button>
+            <button type="button" class="btn openModal" data-toggle="modal" data-idx="${idx}"
+                    id="open-modal${idx}" data-target="#cont-del-modal">
+                <img src="images/del.png"style="width:30px;height:30px;">
+            </button>
+            ${line}
+        </li>`);
+    $('#recipe-details').append(`<li id="ing-edit${idx}">${createIngDiv(idx)}</li>`);
+    $(`#ing-edit${idx}`).hide();
+  });
+  $('button.edit-recipes').prop('disabled', false);
+  $('button.edit-recipes').click(function () {
+    const idx = $(this).data('idx');
+    const ing = currentRec.ingredients[idx];
+    // Populate the ingredient's input fields with the current values from currentRec and then show
+    // the input fields; also hide the "line".
+    $(`#quantity${idx}`).val(ing.quantity);
+    $(`#unit${idx}`).val(ing.unit);
+    $(`#name${idx}`).val(ing.name);
+    $('button.edit-recipes').prop('disabled', true);
+    $(`#ing-edit${idx}`).show();
+    $(`#ing-line-container${idx}`).hide();
+  });
+  $('li > div > button.cancel').click(function () {
+    $('button.edit-recipes').prop('disabled', false);
+    ingCancel($(this).data('idx'));
+  });
+  $('li > div > button.apply').click(function () {
+    ingApply($(this).data('idx'));
+  });
+  $('button.openModal').click(function () {
+    currentIdx = $(this).data('idx');
+  });
+}
+
+/*
+  addRecHandler
+
+  Adds a click handler to the buttons of the recipeList.
+
+  Parameters:
+  matchingButtons - The button being referenced
+*/
+function addRecHandler(matchingButtons) {
+  matchingButtons.click(function () {
+    $('#recipe-details-container').show();
+    $('button.active.recipe-list').removeClass('active');
+    $(this).addClass('active');
+    $.get(`/recipes/${$(this).data('id')}`, successOnAjaxOfRecipe);
+  });
 }
 
 /*
@@ -180,16 +182,16 @@ function recipeButtonCallback() {
 function reset() {
   $('#recipe-details-container').hide();
   // On page load, gets the recipe list, and appends as buttons to a ul in left pane.
-  $.get('/recipes', (recipeList) => {
+  $.get('/recipes', (recipeList, status) => {
     $('#recipe-list').empty();
-    recipeList.forEach((recipe) => {
+    recipeList.forEach((recipe, idx, arr) => {
       $('#recipe-list').append(
         `<button class="recipe-list list-group-item list-group-item-action " type="button"
                  data-id="${recipe.id}">${recipe.name}
          </button>`,
       );
     });
-    $('button.recipe-list').click(recipeButtonCallback);
+    addRecHandler($('button.recipe-list'));
   }, 'json');
   currentRec = null;
   currentIdx = null;
@@ -197,6 +199,7 @@ function reset() {
 
 $(document).ready(() => {
   reset();
+  unitList=$.get('/unitList',successOnAjaxOfRecipe);
   $('#renamed-recipe-name').hide();
   $('#add-rec-div').hide();
   $('#ing-div').hide();
@@ -225,13 +228,18 @@ $(document).ready(() => {
       data: JSON.stringify(currentRec),
       contentType: 'application/json',
       dataType: 'json',
-      success: successOnAjaxOfRecipe,
-    });
+    });// .done(function (data) {
+    //      successOnAjaxOfRecipe(data);
+    //    }).fail(function (jqXHR, textStatus) {
+    //      console.log("ERROR");
+    //    });
   });
   $('input.inputIngInfo').keypress(function (event) {
     const idx = $(this).data('idx');
-    if ($(`#quantity${idx}`).val() !== '' && $(`#name${idx}`).val() !== '' && event.keyCode === 13) {
-      $(`#add-ing${idx}`).click();
+    if ($(`#quantity${idx}`).val() !== ''
+    && $(`#name${idx}`).val() !== ''
+    && event.keyCode === 13) {
+      $(`#addIng${idx}`).click();
     }
   });
 
@@ -242,23 +250,21 @@ $(document).ready(() => {
   });
   $('#add-rec').click(() => {
     $('#add-rec-div').hide();
-    currentRec = { name: $('#new-recipe-name').val(), ingredients: [] };
+    const recipe = { name: $('#new-recipe-name').val(), ingredients: [] };
     $.post({
       url: 'recipes',
-      data: JSON.stringify(currentRec),
+      data: JSON.stringify(recipe),
       contentType: 'application/json',
       dataType: 'json',
-      success: (recipe) => {
-        currentRec = recipe;
+      success: (recipe, status) => {
         $('#recipe-list').append(
           `<button class="recipe-list list-group-item list-group-item-action " type="button"
                    data-id="${recipe.id}">${recipe.name}
            </button>`,
         );
         const lastButton = $('button.recipe-list.list-group-item-action:last-child');
-        lastButton.click(recipeButtonCallback);
+        addRecHandler(lastButton);
         lastButton.trigger('click');
-        $('#plus-ing').click();
       },
     });
   });
@@ -267,7 +273,6 @@ $(document).ready(() => {
     $('#name').val('');
     $('#quantity').val('');
     $('#ing-div').show();
-    $('#quantity').focus();
   });
   $('#del-rec').click(() => {
     $.ajax({
@@ -280,7 +285,7 @@ $(document).ready(() => {
     });
   });
   $('#edit-rec-name').click(() => {
-    $('#rec-title').hide();
+    $('#rec-title-container').hide();
     $('#renamed-recipe-name').show();
   });
   $('#apply-rec-rename').click(() => {
@@ -290,18 +295,17 @@ $(document).ready(() => {
       data: JSON.stringify(currentRec),
       contentType: 'application/json',
       dataType: 'json',
-      success(data) {
+      success(data, status) {
         $('button.recipe-list.active').text(data.name);
       },
     });
-    $('#rec-title').show();
+    $('#rec-title-container').show();
     $('#renamed-recipe-name').hide();
-    $('#rename-rec-input').val($('#rec-title').text());
     $('#rec-title').text(currentRec.name);
   });
   $('#cancel-rec-rename').click(() => {
-    $('#rec-Title').show();
+    $('#rec-title-container').show();
     $('#renamed-recipe-name').hide();
-    $('#rename-rec-input').val($('#rec-title').text());
+    $('#rec-title').text(currentRec.name);
   });
 });
