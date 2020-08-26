84a725b14e0ce002679e808305e8a24ec1ebcc32
curiousrohan1 <curiousrohan1@gmail.com>
2020-08-26 16:07:24 -0700

    Add error handling (#11)
    * Added the done and fail handlers for all ajax requests.
    * Added a DuplicateRecipeException class, caught it, and threw it.
    * Added correct fail handling to all ajax requests.
    * Added a <p> in a strategic and good looking place.
    * Made it look good.

---
+++ src/main/java/org/dishorg/dishorg/DuplicateRecipeException.java
@@ -0,0 +1,7 @@
+package org.dishorg.dishorg;
+
+public class DuplicateRecipeException extends RuntimeException {
+    DuplicateRecipeException(Recipe dupRec) {
+        super("A recipe named " + dupRec.getName() + " already exists!");
+    }
+}
---
+++ src/main/java/org/dishorg/dishorg/RestExceptionHandler.java
@@ -0,0 +1,32 @@
+package org.dishorg.dishorg;
+
+import lombok.Getter;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
+    @ExceptionHandler(value = {DuplicateRecipeException.class})
+    protected ResponseEntity<Object> handleConflict(RuntimeException ex, WebRequest request) {
+        return handleExceptionInternal(ex, new ApiError(HttpStatus.BAD_REQUEST, ex.getMessage()),
+                new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+    }
+
+    @Getter
+    private static class ApiError {
+        private final int statusCode;
+        private final HttpStatus status;
+        private final String message;
+
+        public ApiError(HttpStatus status, String message) {
+            this.statusCode = status.value();
+            this.status = status;
+            this.message = message;
+        }
+    }
+}
