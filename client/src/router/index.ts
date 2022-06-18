6ef4aa147ec51db7128bde490370db39f3f88bbf
Rohan Tamhankar <curiousrohan1@gmail.com>
2022-06-18 15:31:43 -0700

    * Make changes to .env file. * Install needed dependencies. * Get authentication buttons * Run into bugs.

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,21 +1,21 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import RecipesView from '../views/RecipesView.vue'
-import HomeView from '../views/HomeView.vue'
-import AboutView from '../views/AboutView.vue'
-import GroceryView from '../views/GroceryView.vue'
-
+import { createRouter, createWebHistory } from "vue-router";
+import RecipesView from "../views/RecipesView.vue";
+import HomeView from "../views/HomeView.vue";
+import AboutView from "../views/AboutView.vue";
+import GroceryView from "../views/GroceryView.vue";
+import Buttons from "../components/buttons/authentication-button.vue";
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: "/",
+      name: "home",
+      component: HomeView,
     },
     {
-      path: '/about',
-      name: 'about',
-      component: AboutView
+      path: "/about",
+      name: "about",
+      component: AboutView,
     },
     // {
     //   path: '/about',
@@ -26,24 +26,28 @@ const router = createRouter({
     //   component: () => import('../views/AboutView.vue')
     // }
     {
-      path: '/make-a-grocery-list',
-      name: 'groceries',
-      component: GroceryView
+      path: "/make-a-grocery-list",
+      name: "groceries",
+      component: GroceryView,
     },
     {
-      path: '/make-a-recipe',
-      name: 'recipes',
-      component: RecipesView
+      path: "/make-a-recipe",
+      name: "recipes",
+      component: RecipesView,
     },
     {
-      path: '/help',
+      path: "/help",
       component: {
         template: `
           <Help></Help>
-        `
-      }
-    }
-  ]
-})
+        `,
+      },
+    },
+    {
+      path: "/signin",
+      component: Buttons,
+    },
+  ],
+});
 
-export default router
+export default router;
